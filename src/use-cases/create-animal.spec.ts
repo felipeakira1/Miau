12d914ec495038db6aa5daaad6c9b8921e280fc0
felipeakira-exe <feakinoz@gmail.com>
2024-10-31 18:45:22 -0300

    feat: Add unit tests for CreateAnimal use case

---
+++ src/use-cases/create-animal.spec.ts
@@ -0,0 +1,44 @@
+import { describe, it, beforeEach, expect } from "vitest"
+import { AnimalsRepository } from "../repositories/animals-repository"
+import { InMemoryAnimalsRepository } from "../repositories/in-memory/in-memory-animals-repository"
+import { CreateAnimalUseCase } from "./create-animal"
+
+
+describe('Create Animal Use Case', async () => {
+    let animalsRepository : AnimalsRepository
+    let sut : CreateAnimalUseCase
+
+    beforeEach(() => {
+        animalsRepository = new InMemoryAnimalsRepository()
+        sut = new CreateAnimalUseCase(animalsRepository)
+    })
+    
+    it('should be able to create an animal with all fields', async () => {
+        const { animal } = await sut.execute({
+            name: 'Ares',
+            species: 'Gato',
+            breed: 'SRA',
+            birthDate: new Date(),
+            weight: 5.0,
+            imageUrl: '/img',
+            ownerId: 1
+        })
+
+        expect(animal).toBeDefined()
+        expect(animal.name).toEqual('Ares')
+    })
+
+    it('should be able to create an animal without optional fields', async () => {
+        const { animal } = await sut.execute({
+            name: 'Ares',
+            species: 'Gato',
+            breed: 'SRA',
+            birthDate: new Date(),
+            weight: 5.0,
+            ownerId: 1
+        })
+
+        expect(animal).toBeDefined()
+        expect(animal.name).toEqual('Ares')
+    })
+})
