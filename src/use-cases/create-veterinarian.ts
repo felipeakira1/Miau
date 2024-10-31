0960163fab3ca209daed6743ae95f8a0c90dc6fb
felipeakira-exe <feakinoz@gmail.com>
2024-10-31 15:49:41 -0300

    feat: Implement CreateVeterinarian use case

---
+++ src/use-cases/create-veterinarian.ts
@@ -0,0 +1,56 @@
+import { User, Veterinarian } from "@prisma/client"
+import { UsersRepository } from "../repositories/users-repository"
+import { VeterinariansRepository } from "../repositories/veterinarians-repository"
+import { hash } from "bcryptjs"
+import { UserAlreadyExists } from "./errors/user-already-exists"
+
+
+interface CreateVeterinarianUseCaseRequest {
+    name: string,
+    email: string,
+    password: string,
+    address?: string,
+    phone?: string,
+    imageUrl?: string
+    crmv: string,
+    speciality: string,
+}
+
+interface CreateVeterinarianUseCaseResponse {
+    user: User
+    veterinarian: Veterinarian
+}
+
+export class CreateVeterinarianUseCase {
+    constructor(
+        private usersRepository : UsersRepository,
+        private veterinariansRepository : VeterinariansRepository
+    ) {}
+
+    async execute({name, email, password, address, phone, imageUrl, crmv, speciality} : CreateVeterinarianUseCaseRequest) : Promise<CreateVeterinarianUseCaseResponse> {
+        const password_hash = await hash(password, 6)
+
+        const userAlreadyExists = await this.usersRepository.retrieveByEmail(email)
+        if(userAlreadyExists) {
+            throw new UserAlreadyExists()
+        }
+
+        const user = await this.usersRepository.create({
+            name,
+            email,
+            password: password_hash,
+            address,
+            phone,
+            role: "VETERINARIAN"
+        })
+
+        const veterinarian = await this.veterinariansRepository.create({
+            crmv,
+            speciality,
+            userId: user.id,
+            imageUrl
+        })
+
+        return { user, veterinarian }
+    }
+}
