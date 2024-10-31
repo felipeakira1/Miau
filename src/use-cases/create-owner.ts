092f64bc3637d518237772b4c9c525cd46484cc5
felipeakira-exe <feakinoz@gmail.com>
2024-10-31 15:16:22 -0300

    feat: Implement CreateOwner use case and user existence error handling

---
+++ src/use-cases/create-owner.ts
@@ -0,0 +1,51 @@
+import { Owner, User } from "@prisma/client";
+import { OwnersRepository } from "../repositories/owners-repository";
+import { hash } from "bcryptjs"
+import { UsersRepository } from "../repositories/users-repository";
+import { UserAlreadyExists } from "./errors/user-already-exists";
+
+interface CreateOwnerUseCaseRequest {
+    name: string,
+    email: string,
+    password: string,
+    phone?: string,
+    address?: string,
+    imageUrl?: string,
+}
+
+interface CreateOwnerUseCaseResponse {
+    user: User,
+    owner: Owner
+}
+
+export class CreateOwnerUseCase {
+    constructor(
+        private usersRepository : UsersRepository,
+        private ownersRepository : OwnersRepository
+    ) {}
+
+    async execute({name, email, phone, password, address, imageUrl} : CreateOwnerUseCaseRequest) : Promise<CreateOwnerUseCaseResponse> {
+        const password_hash = await hash(password, 6)
+
+        const userAlreadyExists = await this.usersRepository.retrieveByEmail(email)
+        if(userAlreadyExists) {
+            throw new UserAlreadyExists()
+        }
+
+        const user = await this.usersRepository.create({
+            name,
+            email,
+            phone,
+            password: password_hash,
+            address,
+            role: "OWNER"
+        })
+
+        const owner = await this.ownersRepository.create({
+            userId: user.id,
+            imageUrl
+        })
+
+        return { user, owner }
+    }
+}
