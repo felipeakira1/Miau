661d8e8459769d29a37b61338df9cdcb6cfe50d9
felipeakira-exe <feakinoz@gmail.com>
2024-11-28 11:30:06 -0300

    feat: add end-to-end tests for profile

---
+++ src/http/controllers/user/profile.spec.ts
@@ -0,0 +1,39 @@
+import request from "supertest"
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import { app } from "../../../app";
+
+describe('Authenticate (e2e)', () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+    it('should be able to get the profile from an owner', async () => {
+        const postResponse = await request(app.server)
+            .post('/owners')
+            .send({
+                name: 'John Doe',
+                email: 'johndoe@example.com',
+                password: '123456',
+                phone: '11999998888',
+                address: 'Rua dos tutores 1'
+            })
+        
+        const authResponse = await request(app.server)
+            .post('/authenticate')
+            .send({
+                email: 'johndoe@example.com',
+                password: '123456'
+            })
+        const token = authResponse.body.token
+        
+        const response = await request(app.server)
+            .get('/profile')
+            .set('Authorization', `Bearer ${token}`)
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body.user.role).toEqual('OWNER')
+    })
+})
