bf2b48f5d42eb7dd8f8e700e7615c3606618d23a
felipeakira1 <f172885@dac.unicamp.br>
2024-12-03 16:08:32 -0300

    feat: implement PrismaAppointmentsRepository

---
+++ src/repositories/prisma/prisma-appointments-repository.ts
@@ -0,0 +1,41 @@
+import { Appointment, Prisma } from "@prisma/client";
+import { prisma } from "../../lib/prisma";
+import { AppointmentsRepository, updateAppointment } from "../appointments-repository";
+
+
+export class PrismaAppointmentsRepository implements AppointmentsRepository{
+    async create(data: Prisma.AppointmentUncheckedCreateInput) {
+        const appointment = prisma.appointment.create({
+            data
+        })
+        return appointment
+    }
+
+    async retrieveRequestedAppointmentsByVeterinarianId(veterinarianId: number): Promise<Appointment[]> {
+        const appointments = prisma.appointment.findMany({
+            where: {
+                veterinarianId,
+                status: 'Solicitado'
+            }
+        })
+        return appointments
+    }
+
+    async retrieveById(id: number): Promise<Appointment | null> {
+        const appointment = prisma.appointment.findUnique({
+            where: {
+                id
+            }
+        })
+        return appointment
+    }
+
+    async update(data: updateAppointment) {
+        const appointment = prisma.appointment.update({
+            where: {
+                id: data.id
+            },
+            data
+        })
+    }
+}
